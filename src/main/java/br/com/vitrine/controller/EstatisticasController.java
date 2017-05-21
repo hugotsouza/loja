b8577744a9b7f3711c97404ad507615e62647172
Hugo Trindade <souzahugo13@outlook.com>
2017-05-21 11:04:15 -0300

    ...

--- src/main/java/br/com/vitrine/controller/EstatisticasController.java
+++ src/main/java/br/com/vitrine/controller/EstatisticasController.java
@@ -8,7 +8,6 @@ import java.sql.SQLException;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import org.apache.tomcat.jdbc.pool.DataSource;
 import org.hibernate.stat.Statistics;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -23,19 +22,11 @@ public class EstatisticasController {
 	@PersistenceContext
 	private EntityManager em;
 	
-	@Autowired
-	private DataSource ds;
 	@Autowired(required=false)
 	private Statistics statistics;
 
 	@GetMapping
 	public String index(Model model) throws SQLException {
-		System.out.println("[Tomcat pool] Implementação do pool: " + ds.getClass().getName());
-		System.out.println("[Tomcat pool] conexões ativas: " + ds.getActive());
-		System.out.println("[Tomcat pool] conexões ociosas: " + ds.getIdle());
-		System.out.println("[Tomcat pool] tamanho inicial: " + ds.getInitialSize());
-		System.out.println("[Tomcat pool] tamanho maximo de conexões ativas: " + ds.getMaxActive());
-		System.out.println("[Tomcat pool] minimo de conexões ociosas" + ds.getMinIdle());
 		return "estatisticas/index";
 	}
 	
